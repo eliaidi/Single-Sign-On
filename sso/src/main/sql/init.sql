6ccf337eb90f59a5f4025b3a6b23e23e35f67621
xingyue.wang <xingyue.wang@sumscope.com>
2016-09-18 10:18:44 +0800

    make init.sql file more perfectly

--- sso/src/main/sql/init.sql
+++ sso/src/main/sql/init.sql
@@ -1,23 +1,102 @@
-CREATE TABLE `sso` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2016-09-18 10:16:56
+-- 服务器版本： 5.5.48-log
+-- PHP Version: 5.6.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sso`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sso`
+--
+
+CREATE TABLE IF NOT EXISTS `sso` (
+  `id` bigint(20) unsigned NOT NULL,
   `username` varchar(100) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `modify_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `token` varchar(100) DEFAULT NULL,
-  `platform` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `sso_username_app_IDX` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+  `platform` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `sso`
+--
 
+INSERT INTO `sso` (`id`, `username`, `create_time`, `modify_time`, `token`, `platform`) VALUES
+(1, 'admin', '2016-09-06 02:17:26', '2016-09-05 08:49:04', 'dc6203b98cc34d0ea26f9643d709c889', 'PC'),
+(2, 'admin', '2016-09-06 02:52:37', '2016-09-13 06:20:45', '0ffdd53a50734305acea63f4e9390a46', 'WEB');
 
-CREATE TABLE `user` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) unsigned NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(32) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `modify_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `status` tinyint(4) NOT NULL COMMENT '-1:invalid, 0:valid"',
-  PRIMARY KEY (`id`),
-  KEY `user_username_IDX` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+  `status` tinyint(4) NOT NULL COMMENT '-1:invalid, 0:valid"'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `create_time`, `modify_time`, `status`) VALUES
+(1, 'admin', '123456', '2016-09-05 08:17:51', '2016-09-05 08:16:57', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sso`
+--
+ALTER TABLE `sso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sso_username_app_IDX` (`username`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_username_IDX` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sso`
+--
+ALTER TABLE `sso`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
